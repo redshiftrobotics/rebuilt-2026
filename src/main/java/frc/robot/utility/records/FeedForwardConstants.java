8a9bc7ee0df003b29c6343cc1df67b0ac9ab2336
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2025-12-05 12:55:42 -0800

    improve constants records

--- src/main/java/frc/robot/utility/records/FeedForwardConstants.java
+++ src/main/java/frc/robot/utility/records/FeedForwardConstants.java
@@ -1,13 +1,3 @@
 package frc.robot.utility.records;
 
-import com.ctre.phoenix6.configs.Slot0Configs;
-
-public record FeedForwardConstants(double kS, double kV, double kA, double kG) {
-  public FeedForwardConstants(double kS, double kV, double kA) {
-    this(kS, kV, kA, 0.0);
-  }
-
-  public FeedForwardConstants(Slot0Configs slot0Configs) {
-    this(slot0Configs.kS, slot0Configs.kV, slot0Configs.kA, slot0Configs.kG);
-  }
-}
+public record FeedForwardConstants(double kS, double kV, double kA) {}
--- src/main/java/frc/robot/utility/records/PIDConstants.java
+++ src/main/java/frc/robot/utility/records/PIDConstants.java
@@ -1,12 +1,6 @@
 package frc.robot.utility.records;
 
-import com.ctre.phoenix6.configs.Slot0Configs;
-
 public record PIDConstants(double kP, double kI, double kD) {
-  public PIDConstants(Slot0Configs slot0Configs) {
-    this(slot0Configs.kP, slot0Configs.kI, slot0Configs.kD);
-  }
-
   public com.pathplanner.lib.config.PIDConstants toPathPlannerPIDConstants() {
     return new com.pathplanner.lib.config.PIDConstants(kP, kI, kD);
   }
