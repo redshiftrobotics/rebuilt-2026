6257513d9e5a4ef849b34992a9aaf8ea592c3fb6
Aceius E. <aceiuselms@seattleacademy.org>
2026-01-12 18:15:46 -0800

    Start 2026 migration

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "Rebuilt";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 278;
+    public static final String GIT_SHA = "f5b9102a35d31692c7dc18ee27d3e0003f64ec1c";
+    public static final String GIT_DATE = "2026-01-07 22:28:29 PST";
+    public static final String GIT_BRANCH = "main";
+    public static final String BUILD_DATE = "2026-01-12 17:42:02 PST";
+    public static final long BUILD_UNIX_TIME = 1768268522630L;
+    public static final int DIRTY = 1;
+
+    private BuildConstants(){}
+}
