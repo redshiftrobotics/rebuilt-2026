adae7966cc6353b6de490efd60bdfad2534e5d1a
bforcum <113265535+bforcum@users.noreply.github.com>
2026-01-12 23:06:22 -0800

    Fix AdvantageKit annotationProcessor

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "rebuilt-2026";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 279;
-    public static final String GIT_SHA = "6257513d9e5a4ef849b34992a9aaf8ea592c3fb6";
-    public static final String GIT_DATE = "2026-01-12 18:15:46 PST";
+    public static final int GIT_REVISION = 280;
+    public static final String GIT_SHA = "ccb9f5112b6d41214b835e55488ef15b22e71051";
+    public static final String GIT_DATE = "2026-01-12 22:59:02 PST";
     public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2026-01-12 22:43:35 PST";
-    public static final long BUILD_UNIX_TIME = 1768286615115L;
+    public static final String BUILD_DATE = "2026-01-12 23:05:36 PST";
+    public static final long BUILD_UNIX_TIME = 1768287936420L;
     public static final int DIRTY = 1;
 
     private BuildConstants(){}
