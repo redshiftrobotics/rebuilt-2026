b6701f561006e01c887ca23b2816bee9c9e6ae21
ryderabreu <ryderabreu@seattleacademy.org>
2026-01-29 15:48:35 -0800

    Added GEAR_RATIO to intakeconstants

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -7,7 +7,7 @@ public class IntakeConstants {
 
   // Example of a constant that is not dependent on the robot
   public static final double SPEED = 0.5;
-
+  public static final double GEAR_RATIO = 1;
   // Example of a constant that is dependent on the robot
   public static final int CAN_ID =
       switch (Constants.getRobot()) {
--- src/main/java/frc/robot/subsystems/intake/IntakeWheelIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeWheelIOSparkMax.java
@@ -2,8 +2,6 @@ package frc.robot.subsystems.intake;
 
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
-import com.revrobotics.spark.ClosedLoopSlot;
-import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.PersistMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
@@ -12,9 +10,9 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.util.Units;
 import frc.robot.subsystems.intake.IntakeWheelIO.IntakeWheelIOInputsAutoLogged;
+import frc.robot.subsystems.intake.IntakeConstants;
 
 public class IntakeWheelIOSparkMax implements IntakeWheelIO {
-  double GEAR_RATIO = 1;
   private final SparkMax motor = new SparkMax(0, MotorType.kBrushless);
   private final SparkBaseConfig config;
   private final RelativeEncoder encoder;
@@ -30,9 +28,9 @@ public class IntakeWheelIOSparkMax implements IntakeWheelIO {
 
   @Override
   public void updateInputs(IntakeWheelIOInputsAutoLogged inputs) {
-    inputs.positionRad = Units.rotationsToRadians(encoder.getPosition() / GEAR_RATIO);
+    inputs.positionRad = Units.rotationsToRadians(encoder.getPosition() / IntakeConstants.GEAR_RATIO);
     inputs.velocityRadPerSec =
-        Units.rotationsPerMinuteToRadiansPerSecond(encoder.getVelocity() / GEAR_RATIO);
+        Units.rotationsPerMinuteToRadiansPerSecond(encoder.getVelocity() / IntakeConstants.GEAR_RATIO);
     inputs.appliedVolts =
         new double[] {
           motor.getAppliedOutput() * motor.getBusVoltage(),
