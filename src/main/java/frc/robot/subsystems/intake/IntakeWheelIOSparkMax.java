3bab45863b541271fec229bfbeabefb0f499716d
ryderabreu <ryderabreu@seattleacademy.org>
2026-01-29 15:28:42 -0800

    fixed deprecation issue in intakewheeliosparkmax

--- src/main/java/frc/robot/subsystems/intake/IntakeWheelIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeWheelIOSparkMax.java
@@ -1,10 +1,10 @@
 package frc.robot.subsystems.intake;
 
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.ResetMode;
 import com.revrobotics.spark.ClosedLoopSlot;
 import com.revrobotics.spark.SparkBase.ControlType;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.PersistMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig;
