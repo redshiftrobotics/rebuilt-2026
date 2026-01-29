b204f71e2346be27f415ab88521ac9960ad6d795
ryderabreu <ryderabreu@seattleacademy.org>
2026-01-29 12:27:27 -0800

    Created most of intakewheeliosparkmax

--- src/main/java/frc/robot/subsystems/intake/IntakeWheelIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeWheelIOSparkMax.java
@@ -1,3 +1,50 @@
 package frc.robot.subsystems.intake;
 
-public class IntakeWheelIOSparkMax {}
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.math.util.Units;
+import frc.robot.subsystems.intake.IntakeWheelIO.IntakeWheelIOInputsAutoLogged;
+
+public class IntakeWheelIOSparkMax implements IntakeWheelIO {
+  double GEAR_RATIO = 1;
+  private final SparkMax motor = new SparkMax(0, MotorType.kBrushless);
+  private final SparkBaseConfig config;
+  private final RelativeEncoder encoder;
+
+  public IntakeWheelIOSparkMax() {
+    encoder = motor.getEncoder();
+
+    config = new SparkMaxConfig();
+    config.idleMode(IdleMode.kBrake);
+
+    motor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+  }
+
+  @Override
+  public void updateInputs(IntakeWheelIOInputsAutoLogged inputs) {
+    inputs.positionRad = Units.rotationsToRadians(encoder.getPosition() / GEAR_RATIO);
+    inputs.velocityRadPerSec =
+        Units.rotationsPerMinuteToRadiansPerSecond(encoder.getVelocity() / GEAR_RATIO);
+    inputs.appliedVolts =
+        new double[] {
+          motor.getAppliedOutput() * motor.getBusVoltage(),
+        };
+    inputs.supplyCurrentAmps = new double[] {motor.getOutputCurrent()};
+  }
+
+  @Override
+  public void setVelocity(double velocityRadPerSec) {
+
+  }
+
+  @Override
+  public void stop() {
+    motor.set(0);
+  }
+}
