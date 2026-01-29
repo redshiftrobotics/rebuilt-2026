a0ee9d06bd439b94f5c12c052e1c1a4d00c1f3c9
EthanElms <ethanelms@seattleacademy.org>
2026-01-28 16:56:54 -0800

    init
    Co-Authored-By: ryderabreu <233174747+ryderabreu@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/intake/IntakeIOHardware.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOHardware.java
@@ -1,39 +1,5 @@
 package frc.robot.subsystems.intake;
 
-import com.revrobotics.PersistMode;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.ResetMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import edu.wpi.first.math.util.Units;
-
-/** Hardware implementation of the TemplateIO. */
 public class IntakeIOHardware implements IntakeIO {
-  private final SparkMax motor;
-  private final RelativeEncoder encoder;
 
-  public IntakeIOHardware() {
-
-    motor = new SparkMax(IntakeConstants.CAN_ID, MotorType.kBrushless);
-
-    SparkMaxConfig leaderConfig = new SparkMaxConfig();
-    leaderConfig.voltageCompensation(12.0).smartCurrentLimit(30).idleMode(IdleMode.kCoast);
-    leaderConfig.encoder.velocityConversionFactor(1.0).velocityConversionFactor(1.0);
-
-    encoder = motor.getEncoder();
-
-    motor.configure(leaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-  }
-
-  @Override
-  public void updateInputs(IntakeIOInputs inputs) {
-    inputs.velocityRadPerSec = Units.rotationsPerMinuteToRadiansPerSecond(encoder.getVelocity());
-  }
-
-  @Override
-  public void setSpeed(double speed) {
-    motor.set(speed);
-  }
 }
--- src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
@@ -1,21 +1,5 @@
 package frc.robot.subsystems.intake;
 
-import edu.wpi.first.math.util.Units;
-
-/** Simulation implementation of the TemplateIO. */
 public class IntakeIOSim implements IntakeIO {
 
-  private double speed = 0.0;
-
-  public IntakeIOSim() {}
-
-  @Override
-  public void updateInputs(IntakeIOInputs inputs) {
-    inputs.velocityRadPerSec = speed * Units.rotationsPerMinuteToRadiansPerSecond(5676);
-  }
-
-  @Override
-  public void setSpeed(double speed) {
-    this.speed = speed;
-  }
 }
