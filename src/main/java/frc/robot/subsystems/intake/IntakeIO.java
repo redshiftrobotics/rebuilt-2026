13b7bae28c7e28c4de87d2e8b71964748abc4d04
EthanElms <ethanelms@seattleacademy.org>
2026-01-28 17:03:14 -0800

    deleting files
    Co-Authored-By: ryderabreu <233174747+ryderabreu@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -1,5 +1,17 @@
 package frc.robot.subsystems.intake;
 
-public interface IntakeIO {
+import org.littletonrobotics.junction.AutoLog;
 
+/** Interface for the IO layers of the Template subsystem. */
+public interface IntakeIO {
+  @AutoLog
+  public static class IntakeIOInputs {
+    public double velocityRadPerSec = 0.0;
+  }
+
+  /** Updates the set of loggable inputs. */
+  public default void updateInputs(IntakeIOInputs inputs) {}
+
+  /** Run open loop at the specified percentage power. */
+  public default void setSpeed(double speed) {}
 }
