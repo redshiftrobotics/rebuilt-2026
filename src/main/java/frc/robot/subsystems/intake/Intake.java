9c5e3e362d2d1b7b1f0844417f254ff076623452
EthanElms <ethanelms@seattleacademy.org>
2026-01-28 17:20:47 -0800

    Files made working
    Co-Authored-By: ryderabreu <233174747+ryderabreu@users.noreply.github.com>

---
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems.intake;
+
+public class Intake {
+    
+    private final IntakeWheelIO wheelIO;
+    private final SlapdownIO SlapdownIO;
+
+    public Intake(){
+
+    }
+}
