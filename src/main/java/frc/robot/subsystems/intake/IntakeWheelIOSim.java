125f7a9634f80307077928c7c5cf54ef389ef262
EthanElms <ethanelms@seattleacademy.org>
2026-01-30 07:37:21 -0800

    fixed ryders stuff

--- src/main/java/frc/robot/subsystems/intake/IntakeWheelIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeWheelIOSim.java
@@ -1,16 +1,14 @@
 package frc.robot.subsystems.intake;
 
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 
 public class IntakeWheelIOSim implements IntakeWheelIO {
-  private DCMotor gearbox;
-  private DCMotorSim sim;
+  private final DCMotorSim sim;
 
-  public IntakeWheelIOSim() {
-    gearbox = DCMotor.getNEO(1);
-    sim = new DCMotorSim(LinearSystemId.createDCMotorSystem(gearbox, IntakeConstants.JKgMetersSquared, IntakeConstants.gearing), gearbox);
+  public IntakeWheelIOSim(DCMotorSim sim) {
+    // gearbox = DCMotor.getNEO(1);
+    // sim = new DCMotorSim(LinearSystemId.createDCMotorSystem(gearbox, IntakeConstants.JKgMetersSquared, IntakeConstants.gearing), gearbox);
+    this.sim = sim;
   }
 
   @Override
