b0e027f1a0170ad71c6383e8cd03dbd8d6046660
EthanElms <ethanelms@seattleacademy.org>
2026-01-30 07:37:30 -0800

    slapdown sim io

--- src/main/java/frc/robot/subsystems/intake/SlapdownIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/SlapdownIOSim.java
@@ -1,10 +1,21 @@
 package frc.robot.subsystems.intake;
 
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+
 public class SlapdownIOSim implements SlapdownIO{
+
+    private final DCMotorSim motor;
+
+    public SlapdownIOSim(DCMotorSim motor) {
+        this.motor = motor;
+    }
 
     @Override
     public void updateInputs(SlapdownIOInputsAutoLogged inputs) {
-        
+        inputs.PositionRad = motor.getAngularPositionRad();
+        inputs.VelocityRadPerSec = motor.getAngularVelocityRadPerSec();
+        inputs.AppliedVolts = new double[] {0.0};
+        inputs.SupplyCurrentAmps = new double[] {motor.getCurrentDrawAmps()};
     }
 
     @Override
@@ -19,6 +30,11 @@ public class SlapdownIOSim implements SlapdownIO{
 
     @Override
     public void setSpeed(double speed) {
-        
+        motor.setInput(speed);
+    }
+
+    @Override
+    public void stopMotor(){
+        motor.setAngularVelocity(0);
     }
 }
