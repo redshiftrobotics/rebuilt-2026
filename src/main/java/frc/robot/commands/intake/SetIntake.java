f8ae5c796b35921541c40c62d0217517749b0f5d
ryderabreu <ryderabreu@seattleacademy.org>
2026-02-02 16:06:56 -0800

    made intake wheel commands

---
+++ src/main/java/frc/robot/commands/intake/SetIntake.java
@@ -0,0 +1,24 @@
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.intake.Intake;
+
+public class SetIntake extends Command {
+    private final Intake intake;
+    private final double speed;
+
+    public SetIntake(Intake intake, double speed) {
+        this.intake = intake;
+        this.speed = speed;
+    }
+
+    @Override
+    public void initialize() {
+        intake.wheelSet(speed);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+}
---
+++ src/main/java/frc/robot/commands/intake/StopIntake.java
@@ -0,0 +1,22 @@
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.intake.Intake;
+
+public class StopIntake extends Command {
+    public final Intake intake;
+
+    public StopIntake(Intake intake) {
+        this.intake = intake;
+    }
+
+    @Override
+    public void initialize() {
+        intake.wheelStop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+}
