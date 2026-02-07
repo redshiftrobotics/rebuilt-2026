6ad6be73508b530136dcd183327d36e7bcef2a71
EthanElms <ethanelms@seattleacademy.org>
2026-02-07 11:22:05 -0800

    constants naming

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -8,55 +8,47 @@ public class IntakeConstants {
 
   public static final IdleMode SLAPDOWN_BRAKE_MODE = IdleMode.kCoast;
   public static final double INTAKE_WHEEL_SPEED = 0.4;
-
-  public static final int WHEEL_GEAR_RATIO =
-      switch (Constants.getRobot()) {
-        case PRESEASON_2026 -> 1;
-        case SIM_BOT -> 1;
-        default -> 1;
-      };
-
-  public static final int SLAPDOWN_GEAR_RATIO =
-      switch (Constants.getRobot()) {
-        case PRESEASON_2026 -> 1;
-        case SIM_BOT -> 1;
-        default -> 1;
-      };
-
-  public static final int WHEEL_CAN_ID =
-      switch (Constants.getRobot()) {
-        case PRESEASON_2026 -> 0;
-        case SIM_BOT -> 0;
-        default -> 0;
-      };
-
-  public static final int SLAPDOWN_CAN_ID =
-      switch (Constants.getRobot()) {
-        case PRESEASON_2026 -> 0;
-        case SIM_BOT -> 0;
-        default -> 0;
-      };
-
-  public static final int SLAPDOWN_KP =
-      switch (Constants.getRobot()) {
-        case PRESEASON_2026 -> 0;
-        case SIM_BOT -> 0;
-        default -> 0;
-      };
-
-  public static final int SLAPDOWN_KI =
-      switch (Constants.getRobot()) {
-        case PRESEASON_2026 -> 0;
-        case SIM_BOT -> 0;
-        default -> 0;
-      };
-
-  public static final int SLAPDOWN_KD =
-      switch (Constants.getRobot()) {
-        case PRESEASON_2026 -> 0;
-        case SIM_BOT -> 0;
-        default -> 0;
-      };
-
   public static final Rotation2d SLAPDOWN_DOWN_SETPOINT = Rotation2d.kZero;
+
+  public static final double WHEEL_GEAR_RATIO = switch (Constants.getRobot()) {
+    case PRESEASON_2026 -> 1;
+    case SIM_BOT -> 1;
+    default -> 1;
+  };
+
+  public static final double SLAPDOWN_GEAR_RATIO = switch (Constants.getRobot()) {
+    case PRESEASON_2026 -> 1;
+    case SIM_BOT -> 1;
+    default -> 1;
+  };
+
+  public static final int INTAKE_WHEEL_CAN_ID = switch (Constants.getRobot()) {
+    case PRESEASON_2026 -> 0;
+    case SIM_BOT -> 0;
+    default -> 0;
+  };
+
+  public static final int SLAPDOWN_CAN_ID = switch (Constants.getRobot()) {
+    case PRESEASON_2026 -> 0;
+    case SIM_BOT -> 0;
+    default -> 0;
+  };
+
+  public static final double SLAPDOWN_KP = switch (Constants.getRobot()) {
+    case PRESEASON_2026 -> 0;
+    case SIM_BOT -> 0;
+    default -> 0;
+  };
+
+  public static final double SLAPDOWN_KI = switch (Constants.getRobot()) {
+    case PRESEASON_2026 -> 0;
+    case SIM_BOT -> 0;
+    default -> 0;
+  };
+
+  public static final double SLAPDOWN_KD = switch (Constants.getRobot()) {
+    case PRESEASON_2026 -> 0;
+    case SIM_BOT -> 0;
+    default -> 0;
+  };
 }
