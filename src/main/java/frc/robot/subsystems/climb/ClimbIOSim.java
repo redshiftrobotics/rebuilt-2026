267124c5c82b1e30dba3cd44e07fee442ced713b
Aceius E. <io@acei.us>
2026-02-13 21:15:28 -0800

    Bad climb sim

--- src/main/java/frc/robot/subsystems/climb/ClimbIOSim.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbIOSim.java
@@ -5,7 +5,14 @@ import edu.wpi.first.math.numbers.N2;
 import edu.wpi.first.math.system.LinearSystem;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.mechanism.LoggedMechanism2d;
+import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d;
+import org.littletonrobotics.junction.mechanism.LoggedMechanismRoot2d;
 
 /** Simulation implementation of the ClimbIO. */
 public class ClimbIOSim implements ClimbIO {
@@ -14,23 +21,43 @@ public class ClimbIOSim implements ClimbIO {
 
   private final DCMotorSim sim;
 
+  // Visualization:
+  private final LoggedMechanism2d mechanism;
+  private final LoggedMechanismRoot2d root;
+  private final LoggedMechanismLigament2d climbArm;
+
   public ClimbIOSim() {
+    // Set up sim
+
     DCMotor motor = DCMotor.getNEO(1);
-    LinearSystem<N2, N1, N2> dcMotorSystem = LinearSystemId.createDCMotorSystem(motor, 0.004,
-        ClimbConstants.GEAR_RATIO);
+    LinearSystem<N2, N1, N2> dcMotorSystem =
+        LinearSystemId.createDCMotorSystem(motor, 0.004, ClimbConstants.GEAR_RATIO);
 
     sim = new DCMotorSim(dcMotorSystem, motor);
+
+    // Set up vis
+
+    mechanism = new LoggedMechanism2d(2, 3);
+    root = mechanism.getRoot("ClimbRoot", 1, 0.5);
+    climbArm = new LoggedMechanismLigament2d("ClimbArm", 2.0, 90, 10, new Color8Bit(Color.kBlue));
+    root.append(climbArm);
   }
 
   @Override
   public void updateInputs(ClimbIOInputs inputs) {
+    // Periodic
     sim.update(0.02);
+    climbArm.setAngle(Units.radiansToDegrees(inputs.positionRad));
 
+    // Log inputs
     inputs.positionRad = sim.getAngularPositionRad();
     inputs.velocityRadPerSec = sim.getAngularVelocityRadPerSec();
-    inputs.appliedVolts = new double[] { sim.getInputVoltage() };
-    inputs.supplyCurrentAmps = new double[] { sim.getCurrentDrawAmps() };
+    inputs.appliedVolts = new double[] {sim.getInputVoltage()};
+    inputs.supplyCurrentAmps = new double[] {sim.getCurrentDrawAmps()};
     inputs.climberDown = isAtBottom();
+
+    // Log visualization
+    Logger.recordOutput("Climb/Visualization", mechanism);
   }
 
   @Override
