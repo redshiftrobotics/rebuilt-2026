a36a15f7cadd0e8943886992134f57a7bca129e4
Aceius E. <aceiuselms@seattleacademy.org>
2026-02-14 15:34:03 -0800

    Log intake visualization

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -4,10 +4,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
-import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
-import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
-import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -15,6 +11,9 @@ import frc.robot.Constants.RobotType;
 import frc.robot.utility.records.PIDConstants;
 import frc.robot.utility.tunable.TunableNumbers.TunablePID;
 import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.mechanism.LoggedMechanism2d;
+import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d;
+import org.littletonrobotics.junction.mechanism.LoggedMechanismRoot2d;
 
 public class Intake extends SubsystemBase {
 
@@ -32,22 +31,22 @@ public class Intake extends SubsystemBase {
   private IntakeWheelIOInputsAutoLogged wheelInputs;
   private SlapdownIOInputsAutoLogged slapdownInputs;
 
-  private Mechanism2d mech = new Mechanism2d(3, 1.5);
-  private MechanismRoot2d root = mech.getRoot("wheel", 1.75, .25);
-  private MechanismLigament2d slapdownArm =
-      root.append(
-          new MechanismLigament2d(
+  private LoggedMechanism2d visualizerMechanism = new LoggedMechanism2d(3, 1.5);
+  private LoggedMechanismRoot2d visualizerRoot = visualizerMechanism.getRoot("wheel", 1.75, .25);
+  private LoggedMechanismLigament2d visualizerSlapdownArm =
+      visualizerRoot.append(
+          new LoggedMechanismLigament2d(
               "slapdownArm",
               0.5,
               IntakeConstants.SLAPDOWN_UP_SETPOINT.getDegrees(),
               10,
               new Color8Bit(Color.kOrange)));
-  private MechanismLigament2d wheelArm1 =
-      slapdownArm.append(
-          new MechanismLigament2d("wheelArm1", 0.09, 90, 10, new Color8Bit(Color.kRed)));
-  private MechanismLigament2d wheelArm2 =
-      slapdownArm.append(
-          new MechanismLigament2d("wheelArm2", 0.09, 180, 10, new Color8Bit(Color.kRed)));
+  private LoggedMechanismLigament2d wheelArm1 =
+      visualizerSlapdownArm.append(
+          new LoggedMechanismLigament2d("wheelArm1", 0.09, 90, 10, new Color8Bit(Color.kRed)));
+  private LoggedMechanismLigament2d wheelArm2 =
+      visualizerSlapdownArm.append(
+          new LoggedMechanismLigament2d("wheelArm2", 0.09, 180, 10, new Color8Bit(Color.kRed)));
 
   public Intake(IntakeWheelIO wheelIO, SlapdownIO slapdownIO) {
     this.wheelIO = wheelIO;
@@ -74,12 +73,11 @@ public class Intake extends SubsystemBase {
 
     slapdownPidConfig.ifChanged(hashCode(), () -> setSlapdownPID(slapdownPidConfig.get()));
 
-    slapdownArm.setAngle(Units.radiansToDegrees(slapdownInputs.positionRad));
+    visualizerSlapdownArm.setAngle(Units.radiansToDegrees(slapdownInputs.positionRad));
     wheelArm1.setAngle(Units.radiansToDegrees(wheelInputs.positionRad));
     wheelArm2.setAngle(Units.radiansToDegrees(wheelInputs.positionRad + Math.PI));
 
-    SmartDashboard.putNumber("Wheel Speed", wheelIO.getSpeed());
-    SmartDashboard.putData(getName() + "/Visualization", mech);
+    Logger.recordOutput(getName() + "/Visualization", visualizerMechanism);
 
     wheelMotorDisconnectedAlert.set(!wheelInputs.motorConnected);
     slapdownMotorDisconnectedAlert.set(!slapdownInputs.motorConnected);
