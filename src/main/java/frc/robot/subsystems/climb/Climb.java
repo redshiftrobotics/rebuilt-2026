7ec3b5da3ae6ad3fe7f43f8e02eb78805e1ead9b
Aceius E. <io@acei.us>
2026-02-13 21:20:56 -0800

    Move vis code

--- src/main/java/frc/robot/subsystems/climb/Climb.java
+++ src/main/java/frc/robot/subsystems/climb/Climb.java
@@ -1,8 +1,13 @@
 package frc.robot.subsystems.climb;
 
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.RobotType;
 import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.mechanism.LoggedMechanism2d;
+import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d;
 
 /**
  * The subsystem that controls the climber mechanism on the robot. Adapted from: {@link
@@ -13,6 +18,10 @@ import org.littletonrobotics.junction.Logger;
 public class Climb extends SubsystemBase {
   private final ClimbIO io;
   private final ClimbIOInputsAutoLogged inputs = new ClimbIOInputsAutoLogged();
+
+  // Visualization:
+  private final LoggedMechanism2d visualizerMechanism;
+  private final LoggedMechanismLigament2d visualizerArm;
 
   /**
    * Creates a new Climb subsystem compatible with the given robot type.
@@ -36,12 +45,23 @@ public class Climb extends SubsystemBase {
    */
   private Climb(ClimbIO io) {
     this.io = io;
+
+    visualizerMechanism = new LoggedMechanism2d(2, 3);
+    visualizerArm =
+        new LoggedMechanismLigament2d("ClimbArm", 2.0, 90.0, 10.0, new Color8Bit(Color.kBlue));
+
+    visualizerMechanism.getRoot("ClimbRoot", 1, 0.5).append(visualizerArm);
   }
 
   @Override
   public void periodic() {
+    // Log inputs
     io.updateInputs(inputs);
     Logger.processInputs("Climb", inputs);
+
+    // Log visualization
+    visualizerArm.setAngle(Units.radiansToDegrees(inputs.positionRad));
+    Logger.recordOutput("Climb/Visualization", visualizerMechanism);
   }
 
   /** Run the motor at a given speed */
--- src/main/java/frc/robot/subsystems/climb/ClimbIOSim.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbIOSim.java
@@ -5,14 +5,7 @@ import edu.wpi.first.math.numbers.N2;
 import edu.wpi.first.math.system.LinearSystem;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
-import edu.wpi.first.wpilibj.util.Color;
-import edu.wpi.first.wpilibj.util.Color8Bit;
-import org.littletonrobotics.junction.Logger;
-import org.littletonrobotics.junction.mechanism.LoggedMechanism2d;
-import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d;
-import org.littletonrobotics.junction.mechanism.LoggedMechanismRoot2d;
 
 /** Simulation implementation of the ClimbIO. */
 public class ClimbIOSim implements ClimbIO {
@@ -20,11 +13,6 @@ public class ClimbIOSim implements ClimbIO {
   private static final double IS_DOWN_THRESHOLD_RADIANS = 0.1;
 
   private final DCMotorSim sim;
-
-  // Visualization:
-  private final LoggedMechanism2d mechanism;
-  private final LoggedMechanismRoot2d root;
-  private final LoggedMechanismLigament2d climbArm;
 
   public ClimbIOSim() {
     // Set up sim
@@ -34,20 +22,12 @@ public class ClimbIOSim implements ClimbIO {
         LinearSystemId.createDCMotorSystem(motor, 0.004, ClimbConstants.GEAR_RATIO);
 
     sim = new DCMotorSim(dcMotorSystem, motor);
-
-    // Set up vis
-
-    mechanism = new LoggedMechanism2d(2, 3);
-    root = mechanism.getRoot("ClimbRoot", 1, 0.5);
-    climbArm = new LoggedMechanismLigament2d("ClimbArm", 2.0, 90, 10, new Color8Bit(Color.kBlue));
-    root.append(climbArm);
   }
 
   @Override
   public void updateInputs(ClimbIOInputs inputs) {
     // Periodic
     sim.update(0.02);
-    climbArm.setAngle(Units.radiansToDegrees(inputs.positionRad));
 
     // Log inputs
     inputs.positionRad = sim.getAngularPositionRad();
@@ -55,9 +35,6 @@ public class ClimbIOSim implements ClimbIO {
     inputs.appliedVolts = new double[] {sim.getInputVoltage()};
     inputs.supplyCurrentAmps = new double[] {sim.getCurrentDrawAmps()};
     inputs.climberDown = isAtBottom();
-
-    // Log visualization
-    Logger.recordOutput("Climb/Visualization", mechanism);
   }
 
   @Override
