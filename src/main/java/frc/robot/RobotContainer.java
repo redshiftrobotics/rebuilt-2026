3350ff819ffbb16b8d12e8fecb93b626402a736f
Aceius E. <io@acei.us>
2026-02-15 12:10:40 -0800

    LED subsystem factory

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -97,10 +97,7 @@ public class RobotContainer {
 
     drive = Drive.create(robotType);
     vision = AprilTagVision.create(robotType, drive);
-    leds =
-        robotType == RobotType.SIM_BOT
-            ? new LEDSubsystem(new LEDStripIOSim(LEDConstants.DEFAULT_PATTERN))
-            : new LEDSubsystem(); // TODO
+    leds = LEDSubsystem.create(robotType);
     hopper = Hopper.create(robotType);
     intake = Intake.create(robotType);
     climb = Climb.create(robotType);
