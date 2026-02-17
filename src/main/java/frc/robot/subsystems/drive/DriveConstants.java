0686336e7cde75fbeee9f44a7c6ae134f991ffd9
Aceius E. <aceiuselms@seattleacademy.org>
2026-02-17 15:58:39 -0800

    TODO

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -62,7 +62,7 @@ public class DriveConstants {
   /** Center of wheel to center of wheel size */
   private static final Translation2d TRACK_SIZE =
       switch (Constants.getRobot()) {
-        case REBUILT_2026 -> throw new UnsupportedOperationException();
+        case REBUILT_2026 -> throw new UnsupportedOperationException(); // TODO get track size (L:22.75-H:21.75)
         case METALBOT_2, SIM_BOT -> new Translation2d(
             MetalbotTwoConstants.FrontLeft.LocationX - MetalbotTwoConstants.BackRight.LocationX,
             MetalbotTwoConstants.FrontLeft.LocationY - MetalbotTwoConstants.BackRight.LocationY);
@@ -80,7 +80,7 @@ public class DriveConstants {
 
   public static final DriveConfig DRIVE_CONFIG =
       switch (Constants.getRobot()) {
-        case REBUILT_2026 -> throw new UnsupportedOperationException();
+        case REBUILT_2026 -> throw new UnsupportedOperationException(); // TODO
         case METALBOT_2, PRESEASON_2026, SIM_BOT -> new DriveConfig(
             TRACK_SIZE,
             BUMPER_TO_BUMPER,
@@ -107,7 +107,7 @@ public class DriveConstants {
 
   public static final int GYRO_CAN_ID =
       switch (Constants.getRobot()) {
-        case REBUILT_2026 -> throw new UnsupportedOperationException();
+        case REBUILT_2026 -> throw new UnsupportedOperationException(); // TODO Get gyro can id
         case REEFSCAPE_2025, CHASSIS_CANNON, WOOD_BOT_2026 -> 40;
         case METALBOT_2 -> MetalbotTwoConstants.DrivetrainConstants.Pigeon2Id;
         case PRESEASON_2026 -> PreseasonConstants.DrivetrainConstants.Pigeon2Id;
@@ -118,7 +118,7 @@ public class DriveConstants {
 
   public static final CANBus CAN_BUS =
       switch (Constants.getRobot()) {
-        case REBUILT_2026 -> throw new UnsupportedOperationException();
+        case REBUILT_2026 -> throw new UnsupportedOperationException(); // TODO get can bus
         case PRESEASON_2026 -> PreseasonConstants.kCANBus;
         case METALBOT_2 -> MetalbotTwoConstants.kCANBus;
         case REEFSCAPE_2025, CHASSIS_CANNON, WOOD_BOT_2026 -> CANBus.roboRIO();
