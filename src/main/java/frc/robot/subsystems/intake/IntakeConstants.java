527014d21cbcb7a920246f38649014d0849c57cd
Aceius E. <aceiuselms@seattleacademy.org>
2026-02-18 14:12:11 -0800

    Fix gear ratios

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -7,19 +7,13 @@ import frc.robot.utility.records.PIDConfig;
 public class IntakeConstants {
   public static final double INTAKE_WHEEL_SPEED = .5;
   public static final boolean INTAKE_WHEEL_INVERTED = false;
-  public static final double INTAKE_WHEEL_GEAR_RATIO = 0.03125;
+  public static final double INTAKE_WHEEL_GEAR_RATIO = 1;
 
+  public static final double SLAPDOWN_GEAR_RATIO = 0.03125;
   public static final boolean SLAPDOWN_WHEEL_INVERTED = false;
   public static final Rotation2d SLAPDOWN_DOWN_SETPOINT = Rotation2d.fromDegrees(0);
   public static final Rotation2d SLAPDOWN_UP_SETPOINT = Rotation2d.fromDegrees(157);
   public static final Rotation2d SLAPDOWN_INCREMENT_SETPOINT = Rotation2d.fromDegrees(5);
-
-  public static final double SLAPDOWN_GEAR_RATIO =
-      switch (Constants.getRobot()) {
-        case PRESEASON_2026 -> 1;
-        case SIM_BOT -> 1;
-        default -> 1;
-      };
 
   public static final int INTAKE_WHEEL_CAN_ID = 5;
 
