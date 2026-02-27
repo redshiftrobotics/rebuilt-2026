5f783b4ceebdbbeb57bb8b91eca0dcc1bd61fdbc
bforcum <113265535+bforcum@users.noreply.github.com>
2026-02-26 17:46:35 -0800

    Update launcher constants

--- src/main/java/frc/robot/subsystems/launcher/LauncherConstants.java
+++ src/main/java/frc/robot/subsystems/launcher/LauncherConstants.java
@@ -5,6 +5,7 @@ import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.KilogramSquareMeters;
 import static edu.wpi.first.units.Units.Kilograms;
 import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.Millimeters;
 import static edu.wpi.first.units.Units.Pounds;
 
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -27,7 +28,7 @@ public class LauncherConstants {
 
   public static final MotorConstants LEFT_CONSTANTS = new MotorConstants(3, 1, false, false, 120);
   public static final MotorConstants CENTER_CONSTANTS = new MotorConstants(4, 1, false, false, 120);
-  public static final MotorConstants RIGHT_CONSTANTS = new MotorConstants(15, 1, true, true, 120);
+  public static final MotorConstants RIGHT_CONSTANTS = new MotorConstants(15, 1, true, false, 120);
 
   public static final PIDConfig FLYWHEEL_PID =
       switch (Constants.getRobot()) {
@@ -44,20 +45,24 @@ public class LauncherConstants {
   public static final double FLYWHEEL_SETPOINT = 0.5;
 
   public static final Distance LAUNCHER_WHEEL_RADIUS = Inches.of(2);
-  public static final Mass LAUNCHER_WHEEL_MASS = Pounds.of(0.3);
+
+  // TODO (may be innaccurate)
+  public static final Mass LAUNCHER_WHEEL_MASS = Pounds.of(2.2);
+  
   public static final MomentOfInertia LAUNCHER_WHEEL_MOI =
       KilogramSquareMeters.of(
-          LAUNCHER_WHEEL_MASS.in(Kilograms)
+          0.5
+              * LAUNCHER_WHEEL_MASS.in(Kilograms)
               * LAUNCHER_WHEEL_RADIUS.in(Meters)
               * LAUNCHER_WHEEL_RADIUS.in(Meters));
 
-  public static final Distance FLYWHEEL_RADIUS = Inches.of(2);
-  public static final Mass FLYWHEEL_MASS = Pounds.of(4);
+  // public static final Distance FLYWHEEL_RADIUS = Inches.of(0);
+  // public static final Mass FLYWHEEL_MASS = Pounds.of(0);
   public static final MomentOfInertia FLYWHEEL_MOI =
-      KilogramSquareMeters.of(
-          FLYWHEEL_MASS.in(Kilograms) * FLYWHEEL_RADIUS.in(Meters) * FLYWHEEL_RADIUS.in(Meters));
+      KilogramSquareMeters.of(0.0007901271);
 
-  public static final MomentOfInertia TOTAL_MOI = LAUNCHER_WHEEL_MOI.plus(FLYWHEEL_MOI);
+  // TODO waiting on flywheel
+  public static final MomentOfInertia TOTAL_MOI = LAUNCHER_WHEEL_MOI;
   public static final Mass FUEL_MASS = Pounds.of(0.5);
 
   public static final TunableNumber LAUNCHER_TUNING_PARAMETER =
@@ -67,22 +72,22 @@ public class LauncherConstants {
   // Give flywheel double  velocity to account for spin. It rolls the ball, rather than pushing
   public static final double LAUNCHER_VELOCITY_MULTIPLIER =
       2
-          * (LauncherConstants.FUEL_MASS.in(Kilograms)
-              + (LauncherConstants.TOTAL_MOI.in(KilogramSquareMeters))
-              + LauncherConstants.FUEL_MASS.in(Kilograms))
-          / LauncherConstants.TOTAL_MOI.in(KilogramSquareMeters);
+          * (FUEL_MASS.in(Kilograms)
+              + (TOTAL_MOI.in(KilogramSquareMeters))
+              + FUEL_MASS.in(Kilograms))
+          / TOTAL_MOI.in(KilogramSquareMeters);
   public static final Distance LAUNCHER_X_OFFSET = Inches.of(-12);
   public static final Distance LAUNCHER_Z_OFFSET = Inches.of(20);
   public static final Distance HUB_Z_OFFSET = Feet.of(6);
 
   public static final HoodType HOOD_TYPE = HoodType.FIXED;
   public static final Rotation2d FIXED_LAUNCH_ANGLE = new Rotation2d(75);
-  public static final Distance HOOD_RADIUS = Inches.of(20);
+  public static final Distance HOOD_RADIUS = Inches.of(7.4144);
 
   public static final Translation2d ACTUATOR_LOCATION =
-      new Translation2d(Inches.of(-12), Inches.of(-4));
-  public static final Distance ACTUATOR_LENGTH_MIN = Inches.of(12);
-  public static final Distance ACTUATOR_EXTENSION = Inches.of(24);
+      new Translation2d(Inches.of(-5.709), Inches.of(-4.784));
+  public static final Distance ACTUATOR_LENGTH_MIN = Inches.of(6.610);
+  public static final Distance ACTUATOR_EXTENSION = Millimeters.of(100);
 
   public static final Distance MIN_DISTANCE = Feet.of(5);
   public static final Distance MAX_DISTANCE = Feet.of(25);
