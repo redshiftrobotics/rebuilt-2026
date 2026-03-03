3ae20495c5b91c85d5cd1f444355c772a6e5f6c4
Aceius E. <aceiuselms@seattleacademy.org>
2026-03-02 17:40:40 -0800

    Rename constant indicating angle

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -20,7 +20,7 @@ public class VisionConstants {
     UNKNOWN,
     TOP,
     TOP_FLAT,
-    TOP_45,
+    TOP_ANGLE,
     BACK,
     LEFT,
     RIGHT
@@ -34,13 +34,13 @@ public class VisionConstants {
   public static final CameraConfig TOP_CAMERA_ANGLED =
       new CameraConfig(
           "neilcam",
-          CameraPositionName.TOP_45,
+          CameraPositionName.TOP_ANGLE,
           new Transform3d(
               new Translation3d(
                   Units.inchesToMeters(0.257),
                   Units.inchesToMeters(10.015),
                   Units.inchesToMeters(19.137)),
-              new Rotation3d(0, Units.degreesToRadians(-45), 0)));
+              new Rotation3d(0, Units.degreesToRadians(-90+70), 0)));
 
   public static final CameraConfig TOP_CAMERA_FLAT =
       new CameraConfig(
