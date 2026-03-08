054e1b5ed1a2bef1029e2224ed0cbdb7d24bcbe1
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2026-03-07 22:54:01 -0800

    add notes

--- src/main/java/frc/robot/subsystems/launcher/HoodIOActuator.java
+++ src/main/java/frc/robot/subsystems/launcher/HoodIOActuator.java
@@ -10,6 +10,8 @@ public class HoodIOActuator implements HoodIO {
   private final Servo actuatorRight = new Servo(LauncherConstants.ACTUATOR_RIGHT_ID);
 
   public HoodIOActuator() {
+    // See Page 9, Sample code
+    // https://wcproducts.info/files/frc/manuals/WCP%20Miniature%20Linear%20Servo%20Actuators%20-%20User%20Guide.pdf
     actuatorLeft.setBoundsMicroseconds(2000, 1800, 1500, 1200, 1000);
     actuatorRight.setBoundsMicroseconds(2000, 1800, 1500, 1200, 1000);
   }
@@ -25,7 +27,12 @@ public class HoodIOActuator implements HoodIO {
   @Override
   public void updateInputs(HoodIOInputs inputs) {
     inputs.isAdjustable = true;
+
+    // Note, this returns the commanded position, not the position that the servo is actually at, as
+    // the servo does not report its own position.
     inputs.positionLeft = actuatorLeft.get();
     inputs.positionRight = actuatorRight.get();
+
+    // TODO: Use slew rate limiter to estimate real position
   }
 }
