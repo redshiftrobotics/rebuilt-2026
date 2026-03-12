2c675e0e7a3f7b04961535a5ac5f7c6ee62db039
Aceius E. <aceiuselms@seattleacademy.org>
2026-03-12 16:49:59 -0700

    Revert "[TEST] Unstable tweaks"
    This reverts commit 09f8bf51e671d4b5a558b0649be9eb0bf27a0f1a.

--- src/main/java/frc/robot/subsystems/launcher/Launcher.java
+++ src/main/java/frc/robot/subsystems/launcher/Launcher.java
@@ -229,12 +229,10 @@ public class Launcher extends SubsystemBase {
         "LauncherTuning/DesiredHoodPosition", runningDesiredState.hoodPosition());
 
     if (running) {
-      // TODO FOR TESTING ONLY 3/9 : FORCE TO KEEP ANGLE WHILE BUILDING INTERP TABLE
-      // hoodIO.setPosition(runningDesiredState.hoodPosition());
+      hoodIO.setPosition(runningDesiredState.hoodPosition());
       setChannelVelocities(runningDesiredState.wheelRadPerSec());
     } else {
-      // TODO FOR TESTING ONLY 3/9 : DISABLE HOOD LOWERING
-      // hoodIO.setPosition(0); // Lower the hood to avoid collisions when not in use
+      hoodIO.setPosition(0); // Lower the hood to avoid collisions when not in use
       stopChannelMotors(); // Coast to slow down
     }
   }
@@ -259,21 +257,6 @@ public class Launcher extends SubsystemBase {
     return new LauncherState(
         channelInputs.stream().mapToDouble(c -> c.velocityRadPerSec).average().orElse(0.0),
         (hoodInputs.positionLeft + hoodInputs.positionRight) / 2);
-  }
-
-  public double getHoodPosition() {
-    if (!MathUtil.isNear(
-        hoodInputs.positionLeft, hoodInputs.positionRight, HOOD_POSITION_TOLERANCE.get())) {
-      System.err.println("LEFT HOOD & RIGHT HOOD NOT ALIGNED!");
-    }
-
-    return hoodInputs.positionLeft;
-  }
-
-  public void setHoodPosition(double _new) {
-    // TODO FOR TESTING ONLY : 3/9 : FORCE SET HOOD POSITION
-    hoodIO.setPosition(_new);
-    SmartDashboard.putNumber("DEBUG HOOD POSITION", _new);
   }
 
   @AutoLogOutput(key = "Launcher/isReady")
