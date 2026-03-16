8b018ffc1486575958705fad884c7fd664c9ae28
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2026-03-15 17:23:13 -0700

    remove print debug

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -276,9 +276,7 @@ public class RobotContainer {
         .onTrue(rumbleController(xbox, 0.3).withTimeout(0.25))
         .whileTrue(drive.run(drive::stopUsingForwardArrangement).withName("Stop and Orient"));
 
-    xbox.y()
-        .whileTrue(SelfDrivingCommands.selfDriveToOtherZone(drive))
-        .onTrue(Commands.runOnce(() -> System.out.println("y pressed")));
+    xbox.y().whileTrue(SelfDrivingCommands.selfDriveToOtherZone(drive));
 
     // Reset the gyro heading
     xbox.start()
