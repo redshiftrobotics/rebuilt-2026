d2cb666092a8647754224210ae301c739188ccea
Aceius E. <aceiuselms@seattleacademy.org>
2026-03-18 13:22:22 -0700

    Squashed commit of the following:
    commit 567c5d26c3225119493b7174adf5644a4b4386ea
    Merge: 92457d2 731e9b1
    Author: michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
    Date:   Wed Mar 18 12:55:13 2026 -0700
    
        Merge branch 'main' into add-back-cameras
    
    commit 92457d265d98d53ae78c0e05d7d0f2e45d2fcd41
    Author: michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
    Date:   Wed Mar 18 01:21:50 2026 -0700
    
        add helpful screenshot links
    
    commit 643c75e01259c1ea1ac8c0fde9237ca5cb49b2f6
    Author: michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
    Date:   Wed Mar 18 01:15:21 2026 -0700
    
        correct name for Onshape
    
    commit 291d5779feec25f2f8752d9a881145a7d0bf98ef
    Author: michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
    Date:   Wed Mar 18 01:14:41 2026 -0700
    
        undo testing change
    
    commit c20f4ca3907a00039b5a18b66ed1bf6dee248e67
    Author: michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
    Date:   Wed Mar 18 01:00:02 2026 -0700
    
        add new cameras, fix name of old camera
    
    commit 6504c43a8090bc0077d35e9e05e8468f1ed32b9c
    Author: michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
    Date:   Wed Mar 18 00:03:46 2026 -0700
    
        rename current camera for clarity and remove old names
    
    commit 469fa28009b1dd44cb12d0dad5d882f183b3dc38
    Author: michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
    Date:   Tue Mar 17 23:59:40 2026 -0700
    
        add camera position finding instructions
    
    commit 9c759c80a92c18da913bf046091e6e23829d44a5
    Author: michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
    Date:   Tue Mar 17 23:58:18 2026 -0700
    
        add calibration instruction

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -16,14 +16,10 @@ public class VisionConstants {
   // https://docs.photonvision.org/en/latest/docs/apriltag-pipelines/coordinate-systems.html
 
   enum CameraPositionName {
-    FRONT,
     UNKNOWN,
-    TOP,
-    TOP_FLAT,
-    TOP_ANGLE,
-    BACK,
-    LEFT,
-    RIGHT
+    LAUNCHER_RIGHT,
+    BACK_SWERVE_RIGHT,
+    BACK_SWERVE_LEFT,
   }
 
   public record CameraConfig(
@@ -31,20 +27,55 @@ public class VisionConstants {
 
   // --- 2026 REBUILT ---
 
-  public static final CameraConfig TOP_CAMERA_ANGLED =
+  // To find camera position, measure from Arducam model's "Lens Location" mate connector to the
+  // center x/y (forward/left) of robot and z (height) being from floor.
+  // If origin is not center of robot on the floor, then add some offset so it is.
+
+  // NOTE: Measure tool might not always display correct signs, check by logging cameraPose of
+  // camera on 3d field with robot and confirming things look right
+
+  // CAD Link:
+  // https://cad.onshape.com/documents/21a71e9819567f3a143f688d/w/9023f69f68be4b352eda45dd/e/00d63fdcbb0aeb3dcf2a86a6
+
+  // Measuring in CAD Screenshot:
+  // https://drive.google.com/file/d/1uEEOQu9T5Yfil7LBZX7JSF_0oiMpAjLz/view?usp=sharing
+
+  // Checking in Advantage Scope Screenshot:
+  // https://drive.google.com/file/d/17MvlNgLyPRM-VlGa2aJv98TVtX5DwhYD/view?usp=sharing
+
+  public static final CameraConfig LAUNCHER_RIGHT_CAMERA =
       new CameraConfig(
           "spencercam",
-          CameraPositionName.TOP_ANGLE,
-          fromOnShape(
-              -10.470732,
-              2.344,
-              16.864 + 3.710392,
-              -20,
-              0)); // x inches was the one we were supposed to invert, right? - aceius
+          CameraPositionName.LAUNCHER_RIGHT,
+          fromOnshape(-10.470732, 2.344, 16.864 + 3.710392, -20, 0));
 
-  public static Transform3d fromOnShape(
+  public static final CameraConfig BACK_SWERVE_LEFT_CAMERA =
+      new CameraConfig(
+          "TODO1", // geraldcam?
+          CameraPositionName.BACK_SWERVE_LEFT,
+          fromOnshape(+10.047429, 10.817071, 4.096002 + 3.710392, -25, 180 - 45));
+
+  public static final CameraConfig BACK_SWERVE_RIGHT_CAMERA =
+      new CameraConfig(
+          "TODO2", // neilcam?
+          CameraPositionName.BACK_SWERVE_RIGHT,
+          fromOnshape(-10.047429, 10.817071, 4.096002 + 3.710392, -25, 180 + 45));
+
+  /**
+   * Convert from OnShape coordinates to PhotonVision coordinates.
+   *
+   * @param xInches inches from center of robot in Onshape's x direction (left- / right+)
+   * @param yInches inches from center of robot in Onshape's y direction (forward- / backward+)
+   * @param zInches inches from floor in Onshape's z direction (up+ /down-)
+   * @param pitchDegrees rotation around robot's left-right axis. 0 when level looking level to
+   *     horizon, negative pitch looking up, positive pitch looking down
+   * @param yawDegrees rotation around robot's front-back axis. 0 when camera is facing forward,
+   *     positive yaw looking left, negative yaw looking right
+   * @return Transform3d in WPILib's coordinate system
+   */
+  public static Transform3d fromOnshape(
       double xInches, double yInches, double zInches, double pitchDegrees, double yawDegrees) {
-    // x and y are flipped because of the different coordinate system conventions between OnShape
+    // x and y are flipped because of the different coordinate system conventions between Onshape
     // and PhotonVision. Y must be negated, corresponding to a 90 degree rotation
     // https://docs.wpilib.org/en/stable/docs/software/basic-programming/coordinate-system.html
     return new Transform3d(
